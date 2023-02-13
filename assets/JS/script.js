08b2a20b58b8a0706c78215f8e7924ef5971e347
thomasjshea <thomasjshea89@gmail.com>
2023-02-13 10:43:54 -0600

    wrote loop for generating password, copied to several conditions

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -4,33 +4,134 @@
 var capitalLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCaseLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var specialCharacter = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "?","@", "[", "]", "^", "_", "`", "{", "}", "|", "~"];
+var specialCharacter = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"];
 
 // Declare function for generatePassword
 function generatePassword() {
 
+  //Declare variable for Password Length
+  var passwordLength = window.prompt("Please select a password length between 8 and 128 characters");
+  if (!passwordLength) {
+    return placeholder;
+  }
+  else if (passwordLength < 8 || passwordLength > 128) {
+    var lengthError = window.alert("Error!\nPlease select a password length between 8 and 128 characters.");
+    return placeholder;
+  }
+
   // Declare variables to be used within the function 
   var includeCapital = window.confirm("Would you like to include Capital Letters?");
   var includeLowerCase = window.confirm("Would you like to use Lower Case Letters?");
-  var includeNumbers = window.confirm ("Would you like to use numbers?");
-  var includeSpecial = window.confirm ("Would you like to use Special Characters?");
- 
+  var includeNumbers = window.confirm("Would you like to use numbers?");
+  var includeSpecial = window.confirm("Would you like to use Special Characters?");
+
   // Write an if statement for password conditions
   // Using Concat method to make new arrays for each condition
+
+  // Include all possible character types
   if (includeCapital && includeLowerCase && includeNumbers && includeSpecial) {
-    var includeAll = capitalLetter.concat(lowerCaseLetter, numbers, specialCharacter)
+    var include = capitalLetter.concat(lowerCaseLetter, numbers, specialCharacter);
+    console.log(include)
+    var confirm = window.confirm("Please Confirm:\nYou would like to include Capital Letters, Lower Case Letters, Numbers and Special Characters?");
+    if(!confirm) {
+      return placeholder;
+    } else {
+      for (var i = 0; i < passwordLength; i++) {
+        password += include[Math.floor(Math.random()*include.length)];
+      }
+      var resultArray= password.split("HTMLTextAreaElement]")
+      var result=resultArray[1]
+      return result;
+    }
+    // Lower Case, Numbers and Special
   } else if (!includeCapital && includeLowerCase && includeNumbers && includeSpecial) {
-    var excludeCapital = lowerCaseLetter.concat(numbers, specialCharacter);
+    var include = lowerCaseLetter.concat(numbers, specialCharacter);
+    var confirm = window.confirm("Please Confirm:\nYou would like to include Lower Case Letters, Numbers and Special Characters?");
+    if(!confirm){
+      return placeholder;
+    } else{
+      for(var i = 0; i < passwordLength; i++) {
+        password += include[Math.floor(Math.random()*include.length)];
+      }
+    var resultArray = password.split("HTMLTextAreaElement]");
+    var result = resultArray[1];
+    return result;
+    }
+    // Numbers and Special
   } else if (!includeCapital && !includeLowerCase && includeNumbers && includeSpecial) {
-    var excludeLetters = numbers.concat(specialCharacter);
+    var include = numbers.concat(specialCharacter);
+    var confirm = window.confirm("Please Confirm:\nYou would like to include Numbers and Special Characters?");
+    if(!confirm) {
+      return placeholder;
+    }
+    else{
+      for(var i = 0; i < passwordLength; i++) {
+        password += include[Math.floor(Math.random()*include.length)];
+      }
+      var resultArray = password.split("HTMLTextAreaElement]");
+      var result = resultArray[1];
+      return result;
+    }
+    // Capital, Numbers and Special
   } else if (includeCapital && !includeLowerCase && includeNumbers && includeSpecial) {
-    var excludeLowerCase = capitalLetter.concat(numbers, specialCharacter);
+    var include = capitalLetter.concat(numbers, specialCharacter);
+    var confirm = window.confirm("Please Confirm:\nYou would like to include Capital Letters, Numbers and Special Characters?");
+    if (!confirm){
+      return placeholder;
+    }
+    else {
+      for(var i = 0; i < passwordLength; i++) {
+        password += include[Math.floor(Math.random()*include.length)];
+      }
+      var resultArray = password.split("HTMLTextAreaElement]");
+      var result = resultArray[1]
+      return result;
+    }
+    // Capital, Lower Case and Special
   } else if (includeCapital && includeLowerCase && !includeNumbers && includeSpecial) {
-    var excludeNumbers = capitalLetter.concat(lowerCaseLetter, specialCharacter);
+    var include = capitalLetter.concat(lowerCaseLetter, specialCharacter);
+    var confirm = window.confirm("Please Confirm:\nYou would like to include Capital Letters, Lower Case Letters and Special Characters?");
+    if (!confirm){
+      return placeholder;
+    }
+    else {
+      for(var i = 0; i < passwordLength; i++) {
+        password += include[Math.floor(Math.random()*include.length)];
+      }
+      var resultArray = password.split("HTMLTextAreaElement]");
+      var result = resultArray[1]
+      return result;
+    }
+    // Capital, Lower Case and Numbers
   } else if (includeCapital && includeLowerCase && includeNumbers && !includeSpecial) {
-    var excludeSpecial = capitalLetter.concat (lowerCaseLetter, numbers);
+    var include = capitalLetter.concat(lowerCaseLetter, numbers);
+    var confirm = window.confirm("Please Confirm:\nYou would like to include Capital Letters, Lower Case Letters and Numbers?");
+    if (!confirm){
+      return placeholder;
+    }
+    else {
+      for(var i = 0; i < passwordLength; i++) {
+        password += include[Math.floor(Math.random()*include.length)];
+      }
+      var resultArray = password.split("HTMLTextAreaElement]");
+      var result = resultArray[1]
+      return result;
+    }
+    // Capital and Lower Case
   } else if (includeCapital && includeLowerCase && !includeNumbers && !includeSpecial) {
-    var lettersOnly = capitalLetter.concat(lowerCaseLetter);
+    var include = capitalLetter.concat(lowerCaseLetter);
+    var confirm = window.confirm("Please Confirm:\nYou would like to include Capital Letters and Lower Case Letters?");
+    if (!confirm){
+      return placeholder;
+    }
+    else {
+      for(var i = 0; i < passwordLength; i++) {
+        password += include[Math.floor(Math.random()*include.length)];
+      }
+      var resultArray = password.split("HTMLTextAreaElement]");
+      var result = resultArray[1]
+      return result;
+    }
   } else if (includeCapital && !includeLowerCase && includeNumbers && !includeSpecial) {
     var capitalNumbers = capitalLetter.concat(numbers);
   } else if (includeCapital && !includeLowerCase && !includeNumbers && includeSpecial) {
@@ -48,18 +149,14 @@ function generatePassword() {
   } else if (!includeCapital && !includeLowerCase && !includeNumbers && includeSpecial) {
     specialCharacter;
   } else {
-    var typeError = window.alert("Error!\nPlease select at least one Characer Type.")
+    var typeError = window.alert("Error!\nPlease select at least one Character Type.")
     return;
   }
 
-  //Declare variable for Password Length
-  var passwordLength = window.prompt("Please select a password length between 8 and 128 characters");
-  if (passwordLength < 8 || passwordLength > 128) {
-    var lengthError = window.alert("Error!\nPlease select a password length between 8 and 128 characters.");
-  }
-  
 
-  
+
+
+
 }
 
 // Get references to the #generate element
@@ -68,10 +165,12 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  var placeholder = document.getElementById("password").placeholder;
+  console.log (placeholder)
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  
 }
 
 // Add event listener to generate button
